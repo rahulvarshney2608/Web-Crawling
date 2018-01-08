e15efe44ecd7a459cfc4527c46849a0f1308f5fb
Rahul Varshney RV <varshneyrahul67@gmail.com>
2018-01-08 19:08:32 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,16 +1,20 @@
 import re
 import urllib.request
 #http://www.dictionary.com/browse/
-url = 'http://www.dictionary.com/browse/'
-word = input('Give Word whose meaning is fo find: ')
-url = url + word
-data = urllib.request.urlopen(url).read()
-data1  = data.decode('utf-8')
-m = re.search('meta name="description" content="',data1)
-start = m.end()
-end = start + 300
-String = data1[start:end]
-m = re.search(' See more.',String)
-end = m.start()
-newString = String[0:end]
-print(newString)
+while True:
+  url = 'http://www.dictionary.com/browse/'
+  word = input('Give Word whose meaning is fo find: ')
+  url = url + word
+  data = urllib.request.urlopen(url).read()
+  data1  = data.decode('utf-8')
+  m = re.search('meta name="description" content="',data1)
+  start = m.end()
+  end = start + 300
+  String = data1[start:end]
+  m = re.search(' See more.',String)
+  end = m.start()
+  newString = String[0:end]
+  print(newString)
+  m = input('Do you Want to Search For another Word [Y/N] :')
+	if m == 'n' or m == 'N':
+		break
